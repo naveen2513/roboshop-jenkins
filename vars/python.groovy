ab59a50eec3bcd81fc5e3932c9a4295df7f38610
naveen p <naveenpendekatla0407@gmail.com>
2024-10-15 20:37:59 +0530

    jenkins job

---
+++ vars/python.groovy
@@ -0,0 +1,75 @@
+def call() {
+    pipeline {
+
+        agent {
+            node {
+                label 'workstation'
+            }
+        }
+
+        options {
+            ansiColor('xterm')
+        }
+
+        environment {
+            NEXUS = credentials('NEXUS')
+        }
+
+        stages {
+
+            stage('Code Quality') {
+                steps {
+//          sh 'ls -l'
+//          sh 'sonar-scanner -Dsonar.projectKey=${component} -Dsonar.host.url=http://172.31.8.27:9000 -Dsonar.login=admin -Dsonar.password=admin123 -Dsonar.qualitygate.wait=true'
+                    sh 'echo Code Quality'
+                }
+            }
+
+            stage('Unit Test Cases') {
+                steps {
+                    sh 'echo Unit tests'
+                    //sh 'python3.6 -m unittest'
+                }
+            }
+
+            stage('CheckMarx SAST Scan') {
+                steps {
+                    sh 'echo Checkmarx Scan'
+                }
+            }
+
+            stage('CheckMarx SCA Scan') {
+                steps {
+                    sh 'echo Checkmarx SCA Scan'
+                }
+            }
+
+            stage('Release Application') {
+                when {
+                    expression {
+                        env.TAG_NAME ==~ ".*"
+                    }
+                }
+                steps {
+                    sh 'echo $TAG_NAME >VERSION'
+                    //sh 'zip -r ${component}-${TAG_NAME}.zip *.ini *.py *.txt VERSION ${schema_dir}'
+                    //sh 'curl -f -v -u ${NEXUS_USR}:${NEXUS_PSW} --upload-file ${component}-${TAG_NAME}.zip http://172.31.82.149:8081/repository/${component}/${component}-${TAG_NAME}.zip'
+                    sh 'aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 633788536644.dkr.ecr.us-east-1.amazonaws.com'
+                    sh 'docker build -t 633788536644.dkr.ecr.us-east-1.amazonaws.com/${component}:${TAG_NAME} .'
+                    sh 'docker push 633788536644.dkr.ecr.us-east-1.amazonaws.com/${component}:${TAG_NAME}'
+                }
+            }
+
+
+        }
+
+        post {
+            always {
+                cleanWs()
+            }
+        }
+
+    }
+
+
+}
---
+++ vars/terraform.groovy
@@ -0,0 +1,44 @@
+def call() {
+    pipeline {
+
+        agent {
+            node {
+                label 'workstation'
+            }
+        }
+
+        options {
+            ansiColor('xterm')
+        }
+
+        parameters {
+            choice(name: 'env', choices: ['dev', 'prod'], description: 'Pick environment')
+            choice(name: 'action', choices: ['apply', 'destroy'], description: 'Pick Action')
+        }
+
+        stages {
+
+            stage('Terraform INIT') {
+                steps {
+                    sh 'terraform init -backend-config=env-${env}/state.tfvars'
+                }
+            }
+
+            stage('Terraform Apply') {
+                steps {
+                    sh 'terraform ${action} -auto-approve -var-file=env-${env}/main.tfvars'
+                }
+            }
+
+        }
+
+        post {
+            always {
+                cleanWs()
+            }
+        }
+
+    }
+
+
+}
